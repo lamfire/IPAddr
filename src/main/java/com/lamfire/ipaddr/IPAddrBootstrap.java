f7d5f1e2a44a6fc87d1f322ecc805dda26382948
lamfire <hayash@163.com>
2019-06-13 12:32:31 +0800

    print startup 0.0.0.0:8899

--- src/main/java/com/lamfire/ipaddr/IPAddrBootstrap.java
+++ src/main/java/com/lamfire/ipaddr/IPAddrBootstrap.java
@@ -1,18 +1,20 @@
 package com.lamfire.ipaddr;
 
+import com.lamfire.logger.Logger;
 import com.lamfire.warden.HttpServer;
 
 /**
- * Created with IntelliJ IDEA.
  * User: linfan
  * Date: 16-1-5
  * Time: 下午2:54
- * To change this template use File | Settings | File Templates.
  */
 public class IPAddrBootstrap {
+    private static final Logger LOGGER = Logger.getLogger("IPAddrBootstrap");
     public static void main(String[] args) throws Exception{
+
         HttpServer http = new HttpServer("0.0.0.0",8899);
         http.registerAll("com.lamfire.ipaddr");
         http.startup();
+        LOGGER.info("startup - 0.0.0.0:8899");
     }
 }
